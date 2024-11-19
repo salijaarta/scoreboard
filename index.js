914cbfef4973cc4bb8544c8c46613922f1364a1a
artasalija <salija.arta@gmail.com>
2024-11-19 13:41:31 +0100

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let score=0
+
+function plus1(){
+    score+=1
+    document.getElementById("home-score").innerText=score
+}
+
+function plus2(){
+    score+=2
+    document.getElementById("home-score").innerText=score
+}
+
+function plus3(){
+    score+=3
+    document.getElementById("home-score").innerText=score
+}
+
+function plus1g(){
+    score+=1
+    document.getElementById("guest-score").innerText=score
+}
+
+function plus2g(){
+    score+=2
+    document.getElementById("guest-score").innerText=score
+}
+
+function plus3g(){
+    score+=3
+    document.getElementById("guest-score").innerText=score
+}
